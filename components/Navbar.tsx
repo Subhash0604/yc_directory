92037f3cc77e018658946bfd394b4720b9b46ff6
Subhash Peddireddy <138444934+Subhash0604@users.noreply.github.com>
2025-02-26 20:33:16 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { auth, signOut, signIn } from "@/auth";
-// import { BadgePlus, LogOut } from "lucide-react";
-// import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 
 const Navbar = async () => {
   const session = await auth();
