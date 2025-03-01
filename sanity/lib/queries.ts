0fb814243eb574b9182f23f75dc9aaef66ad5044
Subhash0604 <subhashchandra26545@gmail.com>
2025-03-01 17:44:50 +0530

    Created new page for Starups

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,14 +1,32 @@
 export const STARTUP_QUERY = 
 `*[_type == 'startup' &&  defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc)
-{
-   _id,
-   title,
-   slug,
-   category,
-   views,
-   description,
-   author -> {
-     _id,name,slug,image,bio
+  {
+    _id,
+    title,
+    slug,
+    category,
+    views,
+    description,
+    author -> {
+      _id,name,slug,image,bio
+    },
+    image
+  }`;
+
+
+export const STARTUP_QUERY_BY_ID = (`*[_type == "startup" && _id == $id][0]{
+
+  _id,
+  title,
+  slug,
+  _createdAt,
+   author-> {
+       _id, name, username, image, bio
    },
-   image
-}`;
+    views,
+    description,
+    category,
+    image,
+    pitch
+  
+}`);
