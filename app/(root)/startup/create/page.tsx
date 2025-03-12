16227bcf8b9f9d4ac2d117fba9d75a7e352edc0d
Subhash0604 <subhashchandra26545@gmail.com>
2025-03-12 20:05:10 +0530

    added  create page

---
+++ app/(root)/startup/create/page.tsx
@@ -0,0 +1,13 @@
+const page = () => {
+  return (
+    <>
+    <section className="pink_container !min-h-[230px]">
+        <h1 className="headings">
+            Submit your Startup
+        </h1>
+    </section>
+    </>
+  )
+}
+
+export default page
