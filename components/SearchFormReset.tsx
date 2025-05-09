d75b710ed6e60297511897269f47d3112f6b8c4c
Subhash Peddireddy <138444934+Subhash0604@users.noreply.github.com>
2025-05-09 21:03:50 +0530

    Update SearchFormReset.tsx

--- components/SearchFormReset.tsx
+++ components/SearchFormReset.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import Link from 'next/link';
 import { X } from 'lucide-react';
 
-
 //this was coming from sanity which fetches the data
 const SearchFormReset = () => {
     const reset = () => {
